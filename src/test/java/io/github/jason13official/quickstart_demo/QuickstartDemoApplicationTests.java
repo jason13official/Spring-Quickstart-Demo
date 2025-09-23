222d2948b346c135ffcf2bfc6cb1f16cce172b5f
jason13official <jason13official@gmail.com>
2025-09-22 21:56:07 -0400

    feat: import proj generated from https://start.spring.io/

---
+++ src/test/java/io/github/jason13official/quickstart_demo/QuickstartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jason13official.quickstart_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
