163f4fafebfd91628b969032b3dce6cd68e7aea2
jason13official <jason13official@gmail.com>
2025-09-26 14:36:17 -0400

    fix: format with google style guide

--- src/main/java/io/github/jason13official/quickstart_demo/QuickstartDemoApplication.java
+++ src/main/java/io/github/jason13official/quickstart_demo/QuickstartDemoApplication.java
@@ -8,51 +8,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * {@link RestController} is for annotating endpoints/port listeners that should be available
- * with web requests (TCP/UDP?)
+ * {@link RestController} is for annotating endpoints/port listeners that should be available with
+ * web requests (TCP/UDP?)
  */
 @SpringBootApplication
 @RestController
 public class QuickstartDemoApplication {
 
-	// so we don't create a new record every request
-	private static final Person JASON = new Person("Jason", 25);
+  // so we don't create a new record every request
+  private static final Person JASON = new Person("Jason", 25);
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		// runs the specified source using default settings; DI of default values to create new object
-		SpringApplication.run(QuickstartDemoApplication.class, args);
-	}
+    // runs the specified source using default settings; DI of default values to create new object
+    SpringApplication.run(QuickstartDemoApplication.class, args);
+  }
 
-	/**
-	 * Quite a lot to unpack here...
-	 * <br />
-	 * {@link GetMapping} maps requests to `/hello` (i.e. `http://localhost:8080/hello`)
-	 * <br />
-	 * {@link RequestParam} expects a value, but provides a safe default if no value was passed
-	 */
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+  private static String asH1(String s) {
+    return "<h1>" + s + "</h1>";
+  }
 
-	/**
-	 * Returning a hard-coded String representation of a Person object
-	 */
-	@GetMapping("/person/manualSerialization")
-	public String personManual() {
-		return asH1(JASON.toPrettyString());
-	}
+  /**
+   * Quite a lot to unpack here... <br /> {@link GetMapping} maps requests to `/hello` (i.e.
+   * `http://localhost:8080/hello`) <br /> {@link RequestParam} expects a value, but provides a safe
+   * default if no value was passed
+   */
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 
-	/**
-	 * Returning a new Person object created from passed values (without {@link RequestParam})
-	 */
-	@GetMapping("/person")
-	public Person person(String name, Integer age) {
-		return new Person(name, age);
-	}
+  /**
+   * Returning a hard-coded String representation of a Person object
+   */
+  @GetMapping("/person/manualSerialization")
+  public String personManual() {
+    return asH1(JASON.toPrettyString());
+  }
 
-	private static String asH1(String s) {
-		return "<h1>" + s + "</h1>";
-	}
+  /**
+   * Returning a new Person object created from passed values (without {@link RequestParam})
+   */
+  @GetMapping("/person")
+  public Person person(String name, Integer age) {
+    return new Person(name, age);
+  }
 }
