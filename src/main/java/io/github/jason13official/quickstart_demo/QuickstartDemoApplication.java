cc698f54c1cb23c5633918564a11588a277b3f6d
jason13official <jason13official@gmail.com>
2025-09-28 03:18:12 -0400

    feat: create status and GET/POST endpoints

--- src/main/java/io/github/jason13official/quickstart_demo/QuickstartDemoApplication.java
+++ src/main/java/io/github/jason13official/quickstart_demo/QuickstartDemoApplication.java
@@ -2,6 +2,7 @@ package io.github.jason13official.quickstart_demo;
 
 import io.github.jason13official.quickstart_demo.impl.Car;
 import io.github.jason13official.quickstart_demo.impl.Person;
+import io.github.jason13official.quickstart_demo.impl.Status;
 import io.github.jason13official.quickstart_demo.impl.Student;
 import java.util.Arrays;
 import java.util.Collection;
@@ -11,6 +12,7 @@ import java.util.Set;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,8 @@ public class QuickstartDemoApplication {
 
   // so we don't create a new record every request
   private static final Person JASON = new Person("Jason", 25);
+
+  private static Status PLAYER_STATUS = Status.RESPAWNING;
 
   public static void main(String[] args) {
 
@@ -89,5 +93,23 @@ public class QuickstartDemoApplication {
   @GetMapping("/student/set")
   public Set<Student> studentSet() {
     return Set.of(studentsForDemo);
+  }
+
+  @GetMapping("status/simple")
+  public Status getSimpleStatus() {
+    return PLAYER_STATUS;
+  }
+
+  @PostMapping("/status/simple")
+  public Status postSimpleState(@RequestParam(value = "index") Integer index) {
+
+    // cycle if no index provided or invalid, else set to index
+    if (index == null || (index < 0) || (index > Status.values().length - 1)) {
+      PLAYER_STATUS = Status.values()[(PLAYER_STATUS.ordinal() + 1) % Status.values().length];
+    } else {
+      PLAYER_STATUS = Status.values()[index];
+    }
+
+    return PLAYER_STATUS;
   }
 }
