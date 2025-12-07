96bf3b20870d1c5fd22a567e9c2db46c2cb3094e
jason13official <jason13official@gmail.com>
2025-12-06 23:15:53 -0500

    detour step 4, prefer module imports

--- src/main/java/io/github/jason13official/quickstart_demo/QuickstartDemoApplication.java
+++ src/main/java/io/github/jason13official/quickstart_demo/QuickstartDemoApplication.java
@@ -12,13 +12,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import module spring.boot;
+import module spring.boot.autoconfigure;
+import module spring.web;
 
 /**
  * {@link RestController} is for annotating endpoints/port listeners that should be available with
