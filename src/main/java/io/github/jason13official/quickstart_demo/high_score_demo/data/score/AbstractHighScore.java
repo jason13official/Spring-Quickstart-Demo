b28cc9f45b7de72df58372c70d95f5dbbe98f96d
jason13official <jason13official@gmail.com>
2026-01-01 14:47:33 -0500

    fix: annotate abstract highscore with JsonTypeInfo

--- src/main/java/io/github/jason13official/quickstart_demo/high_score_demo/data/score/AbstractHighScore.java
+++ src/main/java/io/github/jason13official/quickstart_demo/high_score_demo/data/score/AbstractHighScore.java
@@ -1,8 +1,12 @@
 package io.github.jason13official.quickstart_demo.high_score_demo.data.score;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import io.github.jason13official.quickstart_demo.high_score_demo.data.score.v1.HighScore;
 import io.github.jason13official.quickstart_demo.high_score_demo.data.score.v2.HighScoreV2;
 
+@JsonTypeInfo(use= Id.CLASS, include= As.PROPERTY, property="class")
 public abstract sealed class AbstractHighScore permits HighScore, HighScoreV2 {
 
   private int score;
